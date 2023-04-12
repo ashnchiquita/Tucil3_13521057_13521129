46bdfddd8010387f966bdf722c4786b3c51a1061
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-04-12 23:14:40 +0700

    docs

--- src/frontend/src/class/Test.ts
+++
@@ -1,8 +0,0 @@
-import { GraphMatrix } from "./GraphMatrix";
-import { Path } from "./Path";
-
-// path relatif thd main
-let graph: GraphMatrix = new GraphMatrix("../../config/test.txt");
-console.log(graph.toString());
-let searchPath: Path = graph.searchUCS(7,4);
-console.log(`Polyline:\n${graph.getPolylineName(searchPath)}\nWeight:\n${searchPath.getPrio()}`);
