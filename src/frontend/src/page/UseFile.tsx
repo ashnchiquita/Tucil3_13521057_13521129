b39b34a1195318e59fb9af8b3753f8fb6a9e4e76
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-04-12 12:39:39 +0700

    feat: file input styling

--- src/frontend/src/page/UseFile.tsx
+++ src/frontend/src/page/UseFile.tsx
@@ -1,5 +1,5 @@
-import { useFilePicker } from "use-file-picker";
-import { Box, Text, Flex, Button, Switch } from "@chakra-ui/react"
+import { useFilePicker, FileContent } from "use-file-picker";
+import { Box, Text, Flex, Button, Switch, VStack, HStack, Heading } from "@chakra-ui/react"
 import { PageTemplate } from '../layout/Template';
 import { GraphMatrix } from "../class/GraphMatrix";
 import { Path } from "../class/Path";
@@ -16,7 +16,40 @@ const arrayRange = (start: number = 0, stop: number, step: number = 1) =>
 const Blue = { color: 'blue' }
 const Red = { color: 'red' }
 
-function grapher (fileStr: string, isUCS: boolean = true) {
+function baseMap() {
+  return (
+    <MapContainer center={[-6.893036, 107.610444]} zoom={20} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+  </MapContainer>
+  )
+}
+
+function baseGraph(fileStr: string) {
+  let graph: GraphMatrix = new GraphMatrix(fileStr);
+  let nodes: { [key: number]: Node } = graph.getNodeMap();
+  let nNodes = graph.getLength();
+  let iArr = arrayRange(0,nNodes);
+  let allpolylines = graph.getAllPolyLine();
+  return (
+    <MapContainer center={nodes[0].getCoordinate().getPosArr()} zoom={20} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {iArr.map(i => <Marker position={nodes[i].getCoordinate().getPosArr()}>
+        <Popup>
+          {nodes[i] !== undefined? nodes[i].getName() : "undefined"}
+        </Popup>
+      </Marker>)}
+      <Polyline pathOptions={Blue} positions={allpolylines}/>
+    </MapContainer>
+  )
+}
+
+function route (fileStr: string, isUCS: boolean = true, isBase: boolean) {
   let graph: GraphMatrix = new GraphMatrix(fileStr);
   let searchPath: Path = isUCS ? graph.searchUCS(0,9) : graph.searchAStar(7,3);
   let nodes: { [key: number]: Node } = graph.getNodeMap();
@@ -25,22 +58,36 @@ function grapher (fileStr: string, isUCS: boolean = true) {
   let allpolylines = graph.getAllPolyLine();
   let polylineanswer = graph.getPolylineArr(searchPath);
   return (
-  <MapContainer center={nodes[0].getCoordinate().getPosArr()} zoom={20} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
+    <MapContainer center={nodes[0].getCoordinate().getPosArr()} zoom={20} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
       {iArr.map(i => <Marker position={nodes[i].getCoordinate().getPosArr()}>
         <Popup>
-          {nodes[i].getName()}
+        {nodes[i] === undefined? "undefined" : nodes[i].getName()}
         </Popup>
       </Marker>)}
       <Polyline pathOptions={Blue} positions={allpolylines}/>
-      <Polyline pathOptions={Red} positions={polylineanswer}/>
-  </MapContainer>);}
+      {!isBase ? <Polyline pathOptions={Red} positions={polylineanswer}/> : null}
+    </MapContainer>
+    )
+  }
+
+function allMap(isUploaded: boolean, isRouteFound: boolean, isUCS: boolean, file: FileContent[]) {
+  if (isUploaded && isRouteFound) {
+    return route(file[0].content, isUCS, false);
+  } else if (isUploaded) {
+    return route(file[0].content, isUCS, true);
+  } else {
+    return baseMap();
+  }
+}
 
 export const UseFile = () =>  {
     const [isUCS, setIsUCS] = useState(true);
+    const [isRouteFound, setIsRouteFound] = useState(false);
+
     const [openFileSelector, { filesContent, loading }] = useFilePicker({
       accept: ".txt"
     });
@@ -49,47 +96,98 @@ export const UseFile = () =>  {
       return <div>Loading...</div>;
     }
     return (
-      <PageTemplate title="Choose Input">
-      <Flex
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        h="100vh"
-        position="relative"
-      >
-
-        <Box bg = 'white' borderRadius="20px">
-          <Text fontSize="2xl" fontWeight="bold" color ="black" mx = '10' my = '1' >
-          {filesContent.length == 1 ? filesContent[0].name : "belom upload"}
-          </Text>
-        </Box>
-        <Box pt={10}>
-        <Button
-          onClick={() => openFileSelector()}
-          background="#8669FA"
-          borderRadius="50px"
-          mt='2em'
-          p='1.2em'
-          lineHeight="92%"
-          fontSize='1.5em'
-          _hover={{
-            transform: 'scale(1.1)'
-          }}
-          color = 'white'
+      <PageTemplate title="Text">
+        <Flex
+          w="100%"
+          px={0}
+          py={0}
+          bg="#1A202B"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          flexWrap="wrap"
+          position="sticky"
+          left="0"
+          zIndex="990"
         >
-          Select File
-        </Button>
-        <label>AStar</label>
-        <Switch
-            alignSelf='center'
-            onChange={() => setIsUCS(!isUCS)}
-            checked={isUCS}
+          {allMap(filesContent.length != 0, isRouteFound, isUCS, filesContent)}
+          <Flex
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            h="100vh"
+            position="relative"
+            rowGap={30}
+            // mx='3em'
+            bg="#1A202B"
+            w='30%'
+          >
+          <Flex
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            h="100vh"
+            position="relative"
+            rowGap={30}
+            // mx='3em'
+            bg="#1A202B"
+          >
+            <VStack spacing = {3}>
+              <Box bg = 'white' borderRadius="20px" w = '100%'>
+                <Text fontSize="1em" color ="black" mx = '10' my = '1' >
+                {filesContent.length == 1 ? filesContent[0].name : "No file selected"}
+                </Text>
+              </Box>
+              <Button
+                onClick={() => openFileSelector()}
+                background="#8669FA"
+                borderRadius="20px"
+                mt='0.5em'
+                lineHeight="92%"
+                fontSize='1em'
+                _hover={{
+                  transform: 'scale(1.1)'
+                }}
+                color = 'white'
+              >
+              Select File
+            </Button>
+            </VStack>
+            <VStack spacing={2}>
+              <Heading size = 'md' color ="white" mx = '10' mb = '0.1em' >
+                Algorithm
+              </Heading>
+              <HStack>
+                <Text fontSize="1em" fontWeight="bold" color ="white" >
+                  A*
+                </Text>
+                <Switch colorScheme='purple' size='lg'
+                  alignSelf='center'
+                  onChange={() => setIsUCS(!isUCS)}
+                  checked={isUCS}
+                ></Switch>
+                <Text fontSize="1em" fontWeight="bold" color ="white">
+                  UCS
+                </Text>
+              </HStack>
+            </VStack>
+            <Button
+              onClick={() => setIsRouteFound(true)}
+              background="#8669FA"
+              borderRadius="20px"
+              mt='0.5em'
+              lineHeight="92%"
+              fontSize='1em'
+              _hover={{
+                transform: 'scale(1.1)'
+              }}
+              color = 'white'
             >
-          <label>UCS</label>
-        </Switch>
-        {filesContent.length == 1 ? grapher(filesContent[0].content, isUCS) : <></>}
-        </Box>
-      </Flex>
-    </PageTemplate>
+              Find Route
+            </Button>
+            </Flex>
+          </Flex>
+        </Flex>
+      </PageTemplate>
     );
 }
